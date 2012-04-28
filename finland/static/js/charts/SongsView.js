2ecc605c615d93ae237559f005d28942799a9f50
Jake Trent <trent.jake@gmail.com>
2012-04-28 10:49:30 -0600

    Only client render songs if needed

--- finland/static/js/charts/SongsView.js
+++ finland/static/js/charts/SongsView.js
@@ -2,9 +2,9 @@ define(['charts/Songs', 'tmpl!charts/tmpl/songs'], function (Songs, songsTmpl) {
   return Backbone.View.extend({
     el: '#main',
     initialize: function () {
-//      if (this.$el.find('.pg-songs').size() === 0) {
-      this.render();
-//      }
+      if (this.$el.find('.pg-songs').size() === 0) {
+        this.render();
+      }
     },
     render: function () {
       this.$el.html(songsTmpl(this.collection.toJSON()));
