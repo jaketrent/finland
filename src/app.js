6951c51488d82643e631eb165f4609d23daad824
jaketrent <trent.jake@gmail.com>
2012-09-27 10:47:52 -0600

    Integrate passport (w/ local and flash) to do login

--- src/app.js
+++ src/app.js
@@ -2,7 +2,17 @@ var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  , flash = require('connect-flash');
+
+var passport = require('passport')
+  , LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    done(null, { id: 123, username: 'jaketrent', password: 'jaketrent' });
+  }
+));
 
 var app = express();
 
@@ -11,20 +21,50 @@ app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.favicon());
-  app.use(express.logger('dev'));
+  app.use(express.logger());
+
+  app.use(express.cookieParser());
   app.use(express.bodyParser());
+  app.use(express.session({ secret: 'keyboard cat' }));
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.use(flash());
+
   app.use(express.methodOverride());
   app.use(app.router);
   app.use(require('stylus').middleware(__dirname + '/static'));
   app.use(express.static(path.join(__dirname, 'static')));
 });
 
-app.configure('development', function(){
+app.configure('development', function () {
+  app.use(express.errorHandler({
+    dumpExceptions: true,
+    showStack:true
+  }));
+});
+app.configure('production', function () {
   app.use(express.errorHandler());
+});
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  done(null, { id: 123, username: 'jaketrent', password: 'jaketrent' });
 });
 
 app.get('/', routes.index);
 app.get('/users', user.list);
+app.get('/login', function (req, res) {
+  res.render('login');
+});
+app.post('/login',
+  passport.authenticate('local', { successRedirect: '/',
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true })
+);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
