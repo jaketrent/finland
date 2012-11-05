76ac312fc4598814220c1377114eed8404d769a6
Jake Trent <trent.jake@gmail.com>
2012-11-04 21:30:01 -0700

    mostly successful infinite scroll (last card not animating correctly from rhs)

--- src/static/js/artists/ArtistListView.js
+++ src/static/js/artists/ArtistListView.js
@@ -7,25 +7,62 @@ define(['tmpl!./artistList', './Artists'], function (artistListTmpl, Artists) {
       'click .card': 'advanceCard'
     },
     advanceCard: function (evt) {
-      var $activeCard = $(evt.currentTarget);
-      $activeCard.removeClass('center').addClass('before');
+      var self = this;
+      var $centerCard = $(evt.currentTarget);
+      var winWidth = $(window).width();
+      var movedLastCardToBefore = false;
+
+      $centerCard.removeClass('center').addClass('before');
       var $nextCard = this.$('.card.after').eq(0);
+
+      // put all cards back in queue
+      if ($nextCard.length === 0) {
+
+        movedLastCardToBefore = true;
+        var $befores = this.$('.card.before');
+        $befores.removeClass('before').addClass('after');
+        this.setXTransform($befores, 1, winWidth);
+
+        // re-enable animation
+        var $afters = this.$('.card.after');
+
+        // re-find next card
+        $nextCard = $afters.eq(0);
+      }
+
       $nextCard.removeClass('after').addClass('center');
 
-      var winWidth = $(window).width();
-      this.setXTransform($activeCard, -1, winWidth);
+      if (movedLastCardToBefore) {
+
+        // after transition ended
+        setTimeout(function () {
+
+          // re-put on right hand side
+          self.setXTransform($centerCard, 1, winWidth);
+        }, 301);
+
+      }
+
+      this.setXTransform($centerCard, -1, winWidth);
       this.setXTransform($nextCard, 1, 0);
 
     },
     render: function () {
+      var self = this;
       this.$el.html(artistListTmpl({
         artists: this.artists.toJSON()
       }));
 
       // put 'after' on rhs of the screen
-      var $afters = this.$('.card.after');
+      this.$cards = this.$('.card');
+      var $afters = this.$cards.filter('.after');
       var winWidth = $(window).width();
       this.setXTransform($afters, 1, winWidth);
+
+      // leave time for card style to be set and painted off screen right before turning on animation
+      setTimeout(function () {
+        self.$cards.addClass('transitional fast');
+      }, 1);
     },
     setXTransform: function ($el, multiplier, Xpx) {
       var direction = (multiplier * Xpx);
